994aaeef3c000fc86e4f94ec1bf9183ac690b601
Jeffrey Voltaire <Jeffreyv300@gmail.com>
2022-02-14 11:12:12 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Jeffrey Voltaire on 2/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
